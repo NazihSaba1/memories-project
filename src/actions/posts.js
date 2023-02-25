b464f8324c571453c96cbbc13b5307cddc7abe48
Nazihsaba1 <nazihsaba23@gamil.com>
2023-02-25 22:52:06 +0200

    first commit

---
+++ src/actions/posts.js
@@ -0,0 +1,61 @@
+import * as api from "../api";
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from "../constants/actionTypes";
+
+// ACTION CREATORS
+
+// ACTION TO GET POSTS
+export const getPosts = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPosts();
+    dispatch({ type: FETCH_ALL, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// ACTION TO CREATE POST
+export const createPost = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(post);
+
+    dispatch({ type: CREATE, payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// ACTION TO UPDATE POST
+
+export const updatePost = (id, post) => async (dispatch) => {
+  try {
+    const { data } = await api.updatePost(id, post);
+
+    dispatch({ type: UPDATE, payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// ACTION TO DELETE POST
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+
+    dispatch({ type: DELETE, payload: id });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//ACTION TO LIKE POST
+
+export const likePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id);
+
+    dispatch({ type: UPDATE, payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
+
+import Post from "./Post/Post";
+import useStyles from "./styles";
+
+const Posts = ({ setCurrentId }) => {
+  const posts = useSelector((state) => state.posts);
+
+  const classes = useStyles();
+  return (
+    <>
+      {!posts.length ? (
+        <CircularProgress
+          className={classes.container}
+          container
+          alignItems="stretch"
+          spacing={3}
+        />
+      ) : (
+        <Grid>
+          {posts.map((post) => (
+            <Grid item key={post._id} xs={12} sm={6}>
+              <Post post={post} setCurrentId={setCurrentId} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </>
+  );
+};
+
+export default Posts;
